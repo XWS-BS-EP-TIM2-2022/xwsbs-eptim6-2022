7d29c21eb46eeb1f579a16b0e0ba70428cc21134
Aleksandar <73308871+Aleksandar9999@users.noreply.github.com>
2022-05-04 10:41:45 +0200

    feat: auth service
    * feat(Backend): Register in memory users
    
    * feat(Backend): User registration with mongo db
    
    * feat(Backend): Generate and validate jwt
    
    * fix(Backend): Validate username before registration
    
    Co-authored-by: Aleksandar Brkljac

---
+++ auth_service/handlers/auth_handler.go
@@ -0,0 +1,116 @@
+package handlers
+
+import (
+	"auth_service/store"
+	"encoding/json"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var secretString = []byte("secret_key") //TODO: Use ENV Variable
+type ErrorMessage struct {
+	Message string `json:"message"`
+}
+type JWT struct {
+	Token string `json:"token"`
+}
+type AuthHandler struct {
+	UserStore *store.UsersStore
+}
+
+func InitAuthHandler() *AuthHandler {
+	userStore := store.InitUsersStore()
+	return &AuthHandler{UserStore: userStore}
+}
+
+func (ag *AuthHandler) LoginUser(w http.ResponseWriter, r *http.Request) {
+	user, err := DecodeUser(r)
+	if err != nil {
+		println("Error while parsing json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	dbUser, err := ag.UserStore.FindByUsername(user.Username)
+	if err != nil {
+		fmt.Println("User not found")
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if dbUser.Password == user.Password {
+		tokenStr, err := GenerateJWT(dbUser)
+		if err != nil {
+			fmt.Printf("Token generation failed %s\n", err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		json.NewEncoder(w).Encode(JWT{Token: tokenStr})
+	} else {
+		fmt.Println("Login failed")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func (ag *AuthHandler) AuthorizeJWT(w http.ResponseWriter, r *http.Request) {
+	if r.Header["Authorization"] != nil {
+		tokenStr := strings.Split(r.Header["Authorization"][0], " ")[1]
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("error")
+			}
+			return secretString, nil
+		})
+		if err != nil {
+			fmt.Println("Error")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if token.Valid {
+			fmt.Println("VALID")
+			json.NewEncoder(w).Encode("valid")
+		}
+	}
+}
+
+func (ag *AuthHandler) AddNewUser(w http.ResponseWriter, r *http.Request) {
+	user, err := DecodeUser(r)
+	if _, err := ag.UserStore.FindByUsername(user.Username); err == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorMessage{Message: "Username already in use"})
+		return
+	}
+	if err != nil {
+		println("Error while parsing json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	ag.UserStore.AddNew(user)
+	fmt.Println("Post request")
+}
+
+func (ag *AuthHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+	users := ag.UserStore.FindAll()
+	json.NewEncoder(w).Encode(users)
+}
+
+func DecodeUser(req *http.Request) (store.User, error) {
+	var user store.User
+	err := json.NewDecoder(req.Body).Decode(&user)
+	return user, err
+}
+
+func GenerateJWT(dbUser store.User) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["authorized"] = true
+	claims["username"] = dbUser.Username
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+	tokenStr, err := token.SignedString(secretString)
+	if err != nil {
+		fmt.Errorf("token signing error")
+		return "", err
+	}
+	return tokenStr, nil
+}
---
+++ auth_service/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"auth_service/handlers"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := RegisterRouts()
+	fmt.Println("START Listening")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func RegisterRouts() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	rg := handlers.InitAuthHandler()
+	router.HandleFunc("/api/auth/users", rg.GetAll).Methods("GET")
+	router.HandleFunc("/api/auth/users", rg.AddNewUser).Methods("POST")
+	router.HandleFunc("/api/auth/session", rg.LoginUser).Methods("PUT")
+	router.HandleFunc("/api/auth/session/validations", rg.AuthorizeJWT).Methods("PUT")
+	return router
+}
