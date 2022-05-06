ce3a4988f46bbc9a077d37eee6cef930ae07250f
lenche <lenkaisidora.aleksic@gmail.com>
2022-05-06 22:41:07 +0200

    fix: delete old profile-service

--- profile-service/domain/model.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type AppUser struct {
-	Id      primitive.ObjectID
-	Name    string
-	Surname string
-}
--- profile-service/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-	//config := cfg.NewConfig()
-	//server := startup.NewServer(config)
-	//server.Start()
-}
