1080f159dabdd51cbf5920f7d77fc3e98f8f9780
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2022-05-18 19:43:57 +0200

    feat: Profile service connected wit

--- profile-service/main.go
+++ profile-service/main.go
@@ -1,13 +1,54 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	usersServicePb "github.com/XWS-BS-EP-TIM2-2022/xwsbs-eptim6-2022/common/proto/profile_service"
+	"github.com/gorilla/mux"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	otgo "github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"google.golang.org/grpc"
 	"log"
+	"net"
 	"net/http"
 	"profile-service/handlers"
-
-	"github.com/gorilla/mux"
 )
+
+type Server struct {
+	usersServicePb.UnsafeProfileServiceServer
+	userHandler *handlers.UserHandler
+}
+
+func (s *Server) GetAllUsers(ctx context.Context, in *usersServicePb.EmptyRequest) (*usersServicePb.UsersResponse, error) {
+	return nil, nil
+}
+func (s *Server) AddNewUser(ctx context.Context, in *usersServicePb.UserRequest) (*usersServicePb.UserResponse, error) {
+	return nil, nil
+}
+func (s *Server) AddSkill(ctx context.Context, in *usersServicePb.SkillRequest) (*usersServicePb.UserResponse, error) {
+	return nil, nil
+}
+func (s *Server) AddInterest(ctx context.Context, in *usersServicePb.InterestRequest) (*usersServicePb.UserResponse, error) {
+	return nil, nil
+}
+func (s *Server) AddExperience(ctx context.Context, in *usersServicePb.ExperienceRequest) (*usersServicePb.UserResponse, error) {
+	return nil, nil
+}
+func (s *Server) AddEducation(ctx context.Context, in *usersServicePb.EducationRequest) (*usersServicePb.UserResponse, error) {
+	return nil, nil
+}
+func (s *Server) FollowUser(ctx context.Context, in *usersServicePb.FollowUserRequest) (*usersServicePb.UserResponse, error) {
+	return nil, nil
+}
+func (s *Server) UnFollowUser(ctx context.Context, in *usersServicePb.FollowUserRequest) (*usersServicePb.UserResponse, error) {
+	return nil, nil
+}
+func (s *Server) AcceptFollow(ctx context.Context, in *usersServicePb.FollowUserRequest) (*usersServicePb.UserResponse, error) {
+	return nil, nil
+}
+func (s *Server) RejectFollow(ctx context.Context, in *usersServicePb.FollowUserRequest) (*usersServicePb.UserResponse, error) {
+	return nil, nil
+}
 
 func RegisterRouts() *mux.Router {
 	myRouter := mux.NewRouter().StrictSlash(true)
@@ -33,7 +74,76 @@ func RegisterRouts() *mux.Router {
 }
 
 func main() {
-	router := RegisterRouts()
-	fmt.Println("START Listening")
-	log.Fatal(http.ListenAndServe(":8081", router))
+	//router := RegisterRouts()
+	//fmt.Println("START Listening")
+	//log.Fatal(http.ListenAndServe(":8080", router))
+	// Create a listener on TCP port
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	// Create a gRPC server object
+	s := grpc.NewServer()
+
+	service, err := NewServer()
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+	usersServicePb.RegisterProfileServiceServer(s, service)
+	log.Println("Serving gRPC on 0.0.0.0:8080")
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+	// Create a client connection to the gRPC server we just started
+	// This is where the gRPC-Gateway proxies the requests
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = usersServicePb.RegisterProfileServiceHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: tracingWrapper(gwmux),
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
+}
+func NewServer() (*Server, error) {
+	return &Server{userHandler: handlers.InitUserHandler()}, nil
+}
+
+var grpcGatewayTag = otgo.Tag{Key: string(ext.Component), Value: "grpc-gateway"}
+
+func tracingWrapper(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		parentSpanContext, err := otgo.GlobalTracer().Extract(
+			otgo.HTTPHeaders,
+			otgo.HTTPHeadersCarrier(r.Header))
+		if err == nil || err == otgo.ErrSpanContextNotFound {
+			serverSpan := otgo.GlobalTracer().StartSpan(
+				"ServeHTTP",
+				// this is magical, it attaches the new span to the parent parentSpanContext, and creates an unparented one if empty.
+				ext.RPCServerOption(parentSpanContext),
+				grpcGatewayTag,
+			)
+			r = r.WithContext(otgo.ContextWithSpan(r.Context(), serverSpan))
+			defer serverSpan.Finish()
+		}
+		h.ServeHTTP(w, r)
+	})
 }
