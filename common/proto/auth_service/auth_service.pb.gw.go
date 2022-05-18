75747740e2bb007f2d3239b9b4786ec445767d08
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2022-05-18 15:37:43 +0200

    fix: Fix body for validation in proto file

--- common/proto/auth_service/auth_service.pb.gw.go
+++ common/proto/auth_service/auth_service.pb.gw.go
@@ -119,18 +119,15 @@ func local_request_AuthService_LoginUser_0(ctx context.Context, marshaler runtim
 
 }
 
-var (
-	filter_AuthService_AuthorizeJWT_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_AuthService_AuthorizeJWT_0(ctx context.Context, marshaler runtime.Marshaler, client AuthServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq Token
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AuthService_AuthorizeJWT_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Token); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -143,10 +140,11 @@ func local_request_AuthService_AuthorizeJWT_0(ctx context.Context, marshaler run
 	var protoReq Token
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AuthService_AuthorizeJWT_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Token); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
