90d14a581dc4b60bbad611e8ec709413c1c89f4f
Aleksandar <73308871+Aleksandar9999@users.noreply.github.com>
2022-05-25 19:49:40 +0200

    feat: Create docker files for services and docker compose
    * created docker files
    
    * docker
    
    * fix: Docker files for api gateway and posts service
    
    Co-authored-by: dusansisarica <dusansisarica@gmail.com>
    Co-authored-by: Aleksandar Brkljac

--- auth_service/startup/config/config.go
+++ auth_service/startup/config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "os"
+
 type Config struct {
 	Host                   string
 	GatewayPort            string
@@ -14,15 +16,14 @@ type Config struct {
 
 func NewConfig() *Config {
 	return &Config{
-		GatewayPort:       "8001",               //os.Getenv("GATEWAY_PORT"),
-		GrpcPort:          "8051",               //os.Getenv("GRPC_PORT"),
-		MongoDbUri:        "localhost:27017",    //os.Getenv("MONGODB_URI"),
+		GatewayPort:            os.Getenv("GATEWAY_AUTH_PORT"),
+		GrpcPort:               os.Getenv("GRPC_AUTH_PORT"),
+		MongoDbUri:             os.Getenv("MONGO_DB_URI_AUTH"),
+		Host:                   os.Getenv("AUTH_HOST"),
+		ProfileServiceGrpcHost: os.Getenv("PROFILE_SERVICE_GRPC_HOST"),
+		ProfileServiceGrpcPort: os.Getenv("PROFILE_SERVICE_GRPC_PORT"),
+		SecretKey:              os.Getenv("SECRET_KEY_AUTH"),
 		MongoDbName:       "auth_service_users", //os.Getenv("MONGODB_URI"),
 		MongoDbCollection: "users",              //os.Getenv("MONGODB_URI"),
-
-		Host:                   "localhost",
-		ProfileServiceGrpcHost: "localhost",
-		ProfileServiceGrpcPort: "8007",
-		SecretKey:              "secret-key",
 	}
 }
