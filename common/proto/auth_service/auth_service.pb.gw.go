042dd29443ebe52b083e6dcbef480308decac185
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2022-06-16 21:18:28 +0200

    fix: Body for 2fa token validation

--- common/proto/auth_service/auth_service.pb.gw.go
+++ common/proto/auth_service/auth_service.pb.gw.go
@@ -479,18 +479,15 @@ func local_request_AuthService_Get2FAToken_0(ctx context.Context, marshaler runt
 
 }
 
-var (
-	filter_AuthService_Submit2FAToken_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_AuthService_Submit2FAToken_0(ctx context.Context, marshaler runtime.Marshaler, client AuthServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ActivationTokenMessage
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AuthService_Submit2FAToken_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Token); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -503,10 +500,11 @@ func local_request_AuthService_Submit2FAToken_0(ctx context.Context, marshaler r
 	var protoReq ActivationTokenMessage
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AuthService_Submit2FAToken_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Token); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
